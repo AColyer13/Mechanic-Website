2d9154ad76553887918f7986ae9e6ae734f8c3f1
Adam Colyer <adamcolyer@gmail.com>
2025-11-26 20:38:13 -0600

    seperate html and js

---
+++ docs/index.js
@@ -0,0 +1,5 @@
+// Mechanic Shop Portal JavaScript
+// This file contains all the JavaScript logic previously embedded in index.html.
+// Paste the full script content from the original <script> tag here.
+
+// ...existing code will be inserted here in the next step...
